e205d30a13c8cc2ad556e83fa59ecc4a00b1059b
yassarmed <yassarahmed17@gmail.com>
2023-09-21 16:12:22 -0500

    messes around with variable data types

---
+++ apostrophe.py
@@ -0,0 +1,11 @@
+# name = "Eric"
+# message = "hello"
+# print("\n name")
+
+# print(name.upper())
+# print(name.lower())
+# print(name.title())
+
+# quote = "we suffer more in our imagination than in reality"
+# author = "Marcus aurelius once said,"
+# print(author, quote)
---
+++ variables.py
@@ -0,0 +1,7 @@
+x = str(10)
+y = int("3")
+z = float(3)
+
+print(x)
+print(y)
+print(z)
