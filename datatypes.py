f82cff84506aca01c9d6b549424a9c7e79a92139
yassarmed <yassarahmed17@gmail.com>
2023-09-24 11:01:34 -0500

    learns about datatypes

---
+++ datatypes.py
@@ -0,0 +1,24 @@
+# lists
+
+list = ["apples", "bananas", "oranges"]
+
+print(len(list))
+
+
+# tuples
+
+tuple = ("apples", "bananas", "oranges")
+
+print(tuple)
+
+
+# sets
+
+set = {"apple", "banana", "orange"}
+
+print(set)
+
+# dictionaries
+
+dictionary = {"brand": "Ford", "model": "escape", "year": "2018"}
+print(dictionary["model"])
