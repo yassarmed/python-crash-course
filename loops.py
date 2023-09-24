b58de2292a7ea49d8dbdabdc75cd43a789da9350
yassarmed <yassarahmed17@gmail.com>
2023-09-24 11:34:44 -0500

    learns about loops' '

---
+++ loops.py
@@ -0,0 +1,21 @@
+# i = 1
+
+# while i < 6:
+#     print(i)
+#     i += 1
+#     if i == 5:
+#         break
+
+# for loops
+
+# veggies = ["spinich", "tomato", "corn"]
+
+# for veggies in veggies:
+#     print(veggies)
+
+fruits = ["apple", "banana", "cabbage"]
+
+for x in fruits:
+    print(x)
+else:
+    print("im done")
